d1f54a07c5041ffdf812fb385e212a8106e853f4
Abhiram <a4abhiram125@gmail.com>
2023-05-30 17:52:41 +0530

    add two numbers

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
-    return render(request,'hello.html')
+	return render(request,'hello.html')
+
+def add(request):
+	val1 = int(request.GET['num1'])
+	val2 = int(request.GET['num2'])
+	res = val1+val2
+	return render(request,'result.html',{'result':res})
+    
 
